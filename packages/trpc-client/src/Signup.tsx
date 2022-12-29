20190c2d49653d134ffe89d20998945af1b09426
arkar <arkar20011@gmail.com>
2022-12-29 21:28:37 +0630

    signup route

--- packages/trpc-client/src/Signup.tsx
+++ packages/trpc-client/src/Signup.tsx
@@ -1,25 +1,55 @@
-import React, { useState } from "react";
+import React, { FormEvent, useMemo, useRef, useState } from "react";
 import "./signup.css";
-
+import { trpc } from "./trpc";
 export default function Signup() {
+  const signUp = trpc.createUser.useMutation();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+
+    signUp.mutateAsync({ email, password });
+  };
+
+  const errors = useMemo(() => {
+    if (signUp.error) {
+      return JSON.parse(signUp.error.message);
+    }
+    return false;
+  }, [signUp.error]);
+
   return (
-    <form>
-      <legend>Sign Up</legend>
-      <input
-        type="text"
-        placeholder="email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button>Signup</button>
-    </form>
+    <>
+      {signUp.isLoading && <p>Loading</p>}
+
+      {errors && (
+        <ul>
+          {errors.map((error: any, index: number) => (
+            <li key={index}>{error.message}</li>
+          ))}
+        </ul>
+      )}
+
+      <form onSubmit={handleSubmit}>
+        <legend>Sign Up</legend>
+        <input
+          type="text"
+          placeholder="email"
+          value={email}
+          onFocus={() => signUp.reset()}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="password"
+          value={password}
+          onFocus={() => signUp.reset()}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button>Signup</button>
+      </form>
+      {signUp.isSuccess && <p>Sign Up successful</p>}
+    </>
   );
 }
--- packages/trpc-server/model/User.model.ts
+++ packages/trpc-server/model/User.model.ts
@@ -1,5 +1,5 @@
-import { t, router, procedure } from "../trpc-setup";
-
+import { router, procedure } from "../trpc-setup";
+import { z } from "zod";
 interface UserType {
   id: string;
   email: string;
@@ -12,6 +12,20 @@ const getAllUsers = router({
   users: procedure.query((): UserType[] => {
     return users;
   }),
+  createUser: procedure
+    .input(
+      z.object({
+        email: z.string().email(),
+        password: z.string(),
+      })
+    )
+    .mutation(({ input }) => {
+      console.log("🚀 ~ file: User.model.ts:23 ~ .mutation ~ input", input);
+
+      return {
+        status: true,
+      };
+    }),
 });
 
 export { getAllUsers };
